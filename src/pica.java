b0563eb147990c263548b763c65552e1a2fb05f7
Mozajevs <alekss.mozajevs@sk.lvt.lv>
2023-03-22 14:12:49 +0200

    Izveidota iespēja izvēlēties Apskatīt ēdienkarti/Pasūtīt picu un apturēt programmu

---
+++ src/pica.java
@@ -0,0 +1,6 @@
+
+public class pica {
+//Atributi
+	 int diametrs;
+	String nosaukums;
+}
--- src/picerija.java
+++ src/picerija.java
@@ -1,9 +1,30 @@
+import javax.swing.JOptionPane;
 
 public class picerija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String izvele;
+		String izvelne[] = {"Apskatît çdienkarti", "Pasûtît picu", "Apturçt" };
+		String piegade[] = {"Saòemt klâtienç", "Piegâdât uz mâjâm"};
+		String piedevas[] = {"Siers", "Síiòíis", "Pepperoni"};
+		String merces[] = {"Tomâtu", "Íiploku", "Èili"};
+		do{
+		izvele = (String) JOptionPane.showInputDialog(null, "Izvçlies darbîbu", "Izvçle", JOptionPane.INFORMATION_MESSAGE, null, izvelne, izvelne[0] );
+		switch (izvele) {
+		case "Apskatît çdienkarti":
+			
+			break;
+		case "Pasûtît picu":
+			
+			break;
+		case "Apturçt": 
+			
+			JOptionPane.showMessageDialog(null, "Programma apturçta!", "Informâcija",JOptionPane.INFORMATION_MESSAGE); break;
+		default:
+			break;
+			}
+		}while(!izvele.equalsIgnoreCase("Apturçt"));
+		System.exit(0);
 	}
 
 }
