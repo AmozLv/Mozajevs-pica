abf1e439a4d9639acd54541723e1f8f873690ad7
unknown <alekss.mozajevs@sk.lvt.lv>
2023-03-23 10:45:18 +0200

    dienas beigas

--- src/pica.java
+++ src/pica.java
@@ -5,13 +5,15 @@ public class pica extends picerija{
 	private String nosaukums;
 	private String piedeva;
 	private String merce;
+	private double cena;
 	
 //konstruktor
-	 pica(String nosaukums, int diametrs, String piedeva, String merce) {
+	 pica(String nosaukums, int diametrs, String piedeva, String merce, double cena) {
 		nosaukums = this.nosaukums;
 		diametrs = this.diametrs;
 		piedeva = this.piedeva;
 		merce = this.merce;
+		cena = this.cena;
 	}
 //metod
 	 public int getDiametrs() {
@@ -26,4 +28,11 @@ public class pica extends picerija{
 	public String getMerce() {
 		 return merce;
 	 }
+	public double getCena() {
+		return cena;
+	}
+	public String picasInfo() {
+		return "Nosaukums: "+nosaukums+"\nDiametrs: "+diametrs+
+				" \nPiedeva: "+piedeva+"\n Merce: "+merce;
+	}
 }
--- src/picerija.java
+++ src/picerija.java
@@ -1,47 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import javax.swing.JOptionPane;
 
 public class picerija {
+	static int Izvele(ArrayList<Object> gatPicas){
+		String[] rSaraksts = new String[gatPicas.size()];
+		for(int i=0; i<rSaraksts.length; i++){
+			rSaraksts[i] = (((pica)gatPicas.get(i)).getNosaukums()+" "+((pica)gatPicas.get(i)).getCena());
+		}
+		String izveletais = (String)JOptionPane.showInputDialog(null, "Kuru izvēlēsies?", "Izvele", JOptionPane.QUESTION_MESSAGE, null, rSaraksts, rSaraksts[0]);
+		return Arrays.asList(rSaraksts).indexOf(izveletais);
+	}
 
 	public static void main(String[] args) {
+		ArrayList<Object> picas = new ArrayList<Object>(); 
+		ArrayList<Object> gatPicas = new ArrayList<Object>(); 
 		int apt=0;
 		int diametri[] = {20, 30, 50};
+		String picasV[] = {"Studentu", "Pepperoni", "Havajas"};
 		Double cenasPicam[]= {5.99, 6.99, 7.99};
+		String piedevas[] = {"Siers ", "Sķiņķis ", "Pepperoni ", "Nav nepieciešams"};
 		Double cenasPiedevam[]= {1.50, 2.00, 2.25};
+		String merces[] = {"Tomātu ", "Asā", "Čili", "Nav nepieciešams"};
 		Double cenasMercem[]= {0.50, 0.75, 1.20};
 		String izvele;
 		String izvelne[] = {"Apskatīt ēdienkarti", "Pasūtīt picu", "Apturēt" };
 		String piegade[] = {"Saņemt klātienē", "Piegādāt uz mājām"};
-		String picas[] = {"Studentu "+cenasPicam[0], "Pepperoni "+cenasPicam[1], "Havajas "+cenasPicam[2]};
-		String piedevas[] = {"Siers "+cenasPiedevam[0], "Sķiņķis "+cenasPiedevam[1], "Pepperoni "+cenasPiedevam[2], "Nav nepieciešams"};
-		String merces[] = {"Tomātu "+cenasMercem[0], "Asā"+cenasMercem[1], "Čili"+cenasMercem[2], "Nav nepieciešams"};
+		
+		gatPicas.add(new pica("Studentu", 20, "Siers", "Tomātu", 5.99));
+		
+		
 		do{
 		izvele = (String) JOptionPane.showInputDialog(null, "Izvēlies darbību", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, izvelne, izvelne[0] );
 		switch (izvele) {
-		case "Apskat�t �dienkarti":
-			JOptionPane.showMessageDialog(null, " ");
-			break;
+		case "Apskatīt ēdienkarti":
+			
+			if(gatPicas.size()>0){
+				
+				String str = "Picu skaits: "+gatPicas.size()+"\n_____________\n";
+				for(int i=0; i<gatPicas.size(); i++){
+					str += ((pica)gatPicas.get(i)).picasInfo()+"\n_____________\n";
+				}
+				
+				JOptionPane.showMessageDialog(null, str, "Datoru saraksts", JOptionPane.INFORMATION_MESSAGE);
+			}else
+				JOptionPane.showMessageDialog(null, "Nav pieejams neviens dators", "Kluda", JOptionPane.ERROR_MESSAGE);
+					break;
 		case "Pasūtīt picu":
 			int atk=0;
 			int diametrs;
-			do {
-				String nosaukums = (String) JOptionPane.showInputDialog(null, "Izv�lies picu", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, picas, picas[0]);
-			do {
+			//do {
+				String nosaukums = (String) JOptionPane.showInputDialog(null, "Izv�lies picu", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, picasV, picasV[0]);
+			//do {
 				 diametrs =  Integer.parseInt(JOptionPane.showInputDialog(null, "Izv�lies picas diametru"));
-			}while(diametrs != 20|| diametrs != 30||diametrs != 50 );
+			//}while(diametrs != 20|| diametrs != 30||diametrs != 50 );
 			//do {
 				String piedeva = (String) JOptionPane.showInputDialog(null, "Izvēlies extra piedevu", "Izvēle", JOptionPane.INFORMATION_MESSAGE, null, piedevas, piedevas[0]);
 				// atk = JOptionPane.showConfirmDialog(null, "Vēlies vēl extra piedevu?", "Papildus?", JOptionPane.YES_NO_OPTION, atk);
 			//} while (atk!= JOptionPane.YES_OPTION);
 			//do {
-				String merce = (String) JOptionPane.showInputDialog(null, "Izv�lies picu", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, piedevas, piedevas[0]);
+				String merce = (String) JOptionPane.showInputDialog(null, "Izv�lies merci", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, merces, merces[0]);
 				//atk = JOptionPane.showConfirmDialog(null, "Vēlies vēl mērci?", "Papildus?", JOptionPane.YES_NO_OPTION, atk);
 			//} while (atk!= JOptionPane.YES_OPTION);
+				double cena = 1.00;
+			 picas.add(new pica(nosaukums, diametrs, piedeva, merce, cena));
+			//atk = JOptionPane.showConfirmDialog(null, "Vēlies pasūtīt vēlvienu picu?", "Vēl?", JOptionPane.YES_NO_OPTION, atk);
+			//} while (atk!= JOptionPane.YES_OPTION);
 			
-			pica p1 = new pica(nosaukums, diametrs, piedeva, merce);
-			atk = JOptionPane.showConfirmDialog(null, "Vēlies pasūtīt vēlvienu picu?", "Vēl?", JOptionPane.YES_NO_OPTION, atk);
-			} while (atk!= JOptionPane.YES_OPTION);
-			
-			String piegadePicas =(String) JOptionPane.showInputDialog(null, "Izv�lies picu", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, piegade, piegade[0]); 
+			String piegadePicas =(String) JOptionPane.showInputDialog(null, "Kā saņemsi?", "Izv�le", JOptionPane.INFORMATION_MESSAGE, null, piegade, piegade[0]); 
+			if(piegadePicas=="Piegādāt uz mājām") {
+				
+			}else JOptionPane.showMessageDialog(null, "Gaidīsim Jūs picērijā pēc 1h");
 			break;
 		case "Apturēt":
 			 apt = JOptionPane.showConfirmDialog(null, "Tiešām vēlies aizvērt programmu?", "Aizvērt?", JOptionPane.YES_NO_OPTION, apt);
